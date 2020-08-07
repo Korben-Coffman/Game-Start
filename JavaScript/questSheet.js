ccabd9f5c0c225ed5a9e05555deb243879daa125
Korben-Coffman <korbencoffman@gmail.com>
2020-08-07 13:39:53 -0700

    Added Status Screen

--- JavaScript/questSheet.js
+++ JavaScript/questSheet.js
@@ -2,18 +2,6 @@ let modal = document.getElementById("quest-Accept-Model");
 let btn = document.getElementById("button-Accept");
 let accept = document.getElementById("Accept");
 
-// accept.addEventListener("click", function () {
-//   if (accept.innerHTML === "Accept Quest") {
-//     document.getElementById("button-Accept").disabled = true;
-//     modal.style.display = "block";
-//     document.getElementById("Decline").innerHTML = "Forget Quest";
-//     accept.innerHTML = "Complete Quest";
-//     setTimeout(() => {
-//       modal.style.display = "none";
-//     }, 1500);
-//   }
-// });
-
 accept.addEventListener("click", acceptQuest);
 
 function acceptQuest() {
